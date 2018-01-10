2b586c2ebc4617e8002cfd935a99a8e742eac5ae
Justyna Wasilewska <malajustine@gmail.com>
2018-01-10 14:38:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  kittens = [...kittens, name]
-  return kittens
+  var newKittens = [...kittens, name]
+  return newKittens
 }
+
